cac6ed7021dddd756c449c90922ff7a1da100f98
Herkules <herkules11282@gmail.com>
2018-06-22 10:18:30 +0700

    Commit master, Versi 1

---
+++ application/config/site.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+| Your site name,
+|
+*/
+$config['site_name'] = 'DLH Prov Kalteng';
+$config['version'] = '2.6.8';
---
+++ application/views/backend/standart/administrator/form_builder/form_test/form_test_update.php
@@ -0,0 +1,346 @@
+
+<script src="<?= BASE_ASSET; ?>/js/jquery.hotkeys.js"></script>
+<script type="text/javascript">
+    function domo(){
+     
+       // Binding keys
+       $('*').bind('keydown', 'Ctrl+s', function assets() {
+          $('#btn_save').trigger('click');
+           return false;
+       });
+    
+       $('*').bind('keydown', 'Ctrl+x', function assets() {
+          $('#btn_cancel').trigger('click');
+           return false;
+       });
+    
+      $('*').bind('keydown', 'Ctrl+d', function assets() {
+          $('.btn_save_back').trigger('click');
+           return false;
+       });
+        
+    }
+    
+    jQuery(document).ready(domo);
+</script>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Test        <small><?= cclang('update'); ?> Test</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class=""><a  href="<?= site_url('administrator/form_test'); ?>">Test</a></li>
+        <li class="active"><?= cclang('update'); ?></li>
+    </ol>
+</section>
+<!-- Main content -->
+<section class="content">
+    <div class="row" >
+        <div class="col-md-12">
+            <div class="box box-warning">
+                <div class="box-body ">
+                    <!-- Widget: user widget style 1 -->
+                    <div class="box box-widget widget-user-2">
+                        <!-- Add the bg color to the header using any of the bg-* classes -->
+                        <div class="widget-user-header ">
+                            <div class="widget-user-image">
+                                <img class="img-circle" src="<?= BASE_ASSET; ?>/img/add2.png" alt="User Avatar">
+                            </div>
+                            <!-- /.widget-user-image -->
+                            <h3 class="widget-user-username">Test</h3>
+                            <h5 class="widget-user-desc">Edit Test</h5>
+                            <hr>
+                        </div>
+                        <?= form_open(base_url('administrator/form_test/edit_save/'.$this->uri->segment(4)), [
+                            'name'    => 'form_form_test', 
+                            'class'   => 'form-horizontal', 
+                            'id'      => 'form_form_test', 
+                            'method'  => 'POST'
+                            ]); ?>
+                         
+                                                <div class="form-group ">
+                            <label for="datetime" class="col-sm-2 control-label">Datetime 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-6">
+                            <div class="input-group date col-sm-8">
+                              <input type="text" class="form-control pull-right datetimepicker" name="datetime" value="<?= set_value('datetime', $form_test->datetime); ?>" id="datetime" >
+                            </div>
+                            <small class="info help-block">
+                            </small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="yes_no" class="col-sm-2 control-label">Yes No 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-6">
+                                <div class="col-md-2 padding-left-0">
+                                    <label>
+                                        <input type="radio" class="flat-red" name="yes_no" id="yes_no"  value="yes" <?= $form_test->yes_no == "yes" ? "checked" : ""; ?> >
+                                        <?= cclang('yes'); ?>
+                                    </label>
+                                </div>
+                                <div class="col-md-14">
+                                    <label>
+                                        <input type="radio" class="flat-red" name="yes_no" id="yes_no"  value="no" <?= $form_test->yes_no == "no" ? "checked" : ""; ?> >
+                                        <?= cclang('no'); ?>
+                                    </label>
+                                </div>
+                                <small class="info help-block">
+                                </small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="input" class="col-sm-2 control-label">Input 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" name="input" value="<?= set_value('input', $form_test->input); ?>" id="input" placeholder=""  >
+                                <small class="info help-block">
+                                </small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="select" class="col-sm-2 control-label">Select 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-8">
+                                <input type="text" class="form-control" name="select" value="<?= set_value('select', $form_test->select); ?>" id="select" placeholder=""  >
+                                <small class="info help-block">
+                                </small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="custom_select" class="col-sm-2 control-label">Custom Select 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-6">
+                                <div class="col-md-2 padding-left-0">
+                                    <label>
+                                        <input type="radio" class="flat-red" name="custom_select" id="custom_select"  value="1"  <?= $form_test->custom_select == "1" ? "checked" : ""; ?>>
+                                        <?= cclang('yes'); ?>
+                                    </label>
+                                </div>
+                                <div class="col-md-14">
+                                    <label>
+                                        <input type="radio" class="flat-red" name="custom_select" id="custom_select"  value="0" <?= $form_test->custom_select == "0" ? "checked" : ""; ?>>
+                                        <?= cclang('no'); ?>
+                                    </label>
+                                </div>
+                                <small class="info help-block">
+                                </small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="custom_select_multiple" class="col-sm-2 control-label">Custom Select Multiple 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-2">
+                                <select  class="form-control chosen chosen-select-deselect" name="custom_select_multiple" id="custom_select_multiple" data-placeholder="Select Custom Select Multiple" >
+                                    <option value=""></option>
+                                    <?php for ($i = 1970; $i < date('Y')+100; $i++){ ?>
+                                    <option <?=  $i ==  $form_test->custom_select_multiple ? 'selected' : ''; ?> value="<?= $i; ?>"><?= $i; ?></option>
+                                    <?php }; ?> 
+                                </select>
+                                <small class="info help-block">
+                                </small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="custom_checkbox" class="col-sm-2 control-label">Custom Checkbox 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-6">
+                            <div class="input-group date col-sm-8">
+                              <input type="text" class="form-control pull-right timepicker" value="<?= set_value('custom_checkbox', $form_test->custom_checkbox); ?>"  name="custom_checkbox" id="custom_checkbox" >
+                            </div>
+                            <small class="info help-block">
+                            </small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="true_false" class="col-sm-2 control-label">True False 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-6">
+                                <div class="col-md-2 padding-left-0">
+                                    <label>
+                                        <input type="radio" class="flat-red" name="true_false" id="true_false"  value="1"  <?= $form_test->true_false == "1" ? "checked" : ""; ?>>
+                                        <?= cclang('yes'); ?>
+                                    </label>
+                                </div>
+                                <div class="col-md-14">
+                                    <label>
+                                        <input type="radio" class="flat-red" name="true_false" id="true_false"  value="0" <?= $form_test->true_false == "0" ? "checked" : ""; ?>>
+                                        <?= cclang('no'); ?>
+                                    </label>
+                                </div>
+                                <small class="info help-block">
+                                </small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="custom_option" class="col-sm-2 control-label">Custom Option 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-6">
+                            <div class="input-group date col-sm-8">
+                              <input type="text" class="form-control pull-right timepicker" value="<?= set_value('custom_option', $form_test->custom_option); ?>"  name="custom_option" id="custom_option" >
+                            </div>
+                            <small class="info help-block">
+                            </small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="options" class="col-sm-2 control-label">Options 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-6">
+                            <div class="input-group date col-sm-8">
+                              <input type="text" class="form-control pull-right datepicker" value="<?= set_value('options', $form_test->options); ?>"  name="options"  placeholder="" id="options" >
+                            </div>
+                            <small class="info help-block">
+                            </small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="checkboxes" class="col-sm-2 control-label">Checkboxes 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-8">
+                                <select  class="form-control chosen chosen-select" name="checkboxes[]" id="checkboxes" data-placeholder="Select Checkboxes" multiple >
+                                    <option value=""></option>
+                                    <?php foreach (db_get_all_data('aauth_login_attempts') as $row): ?>
+                                    <option value="<?= $row->id ?>"><?= $row->id; ?></option>
+                                    <?php endforeach; ?>  
+                                </select>
+                                <small class="info help-block">
+                                </small>
+                            </div>
+                        </div>
+                                                 
+                                                <div class="form-group ">
+                            <label for="select_multiple" class="col-sm-2 control-label">Select Multiple 
+                            <i class="required">*</i>
+                            </label>
+                            <div class="col-sm-6">
+                            <div class="input-group date col-sm-8">
+                              <input type="text" class="form-control pull-right datepicker" value="<?= set_value('select_multiple', $form_test->select_multiple); ?>"  name="select_multiple"  placeholder="" id="select_multiple" >
+                            </div>
+                            <small class="info help-block">
+                            </small>
+                            </div>
+                        </div>
+                                                
+                        <div class="message"></div>
+                        <div class="row-fluid col-md-7">
+                            <button class="btn btn-flat btn-primary btn_save btn_action" id="btn_save" data-stype='stay' title="<?= cclang('save_button'); ?> (Ctrl+s)">
+                            <i class="fa fa-save" ></i> <?= cclang('save_button'); ?>
+                            </button>
+                            <a class="btn btn-flat btn-info btn_save btn_action btn_save_back" id="btn_save" data-stype='back' title="<?= cclang('save_and_go_the_list_button'); ?> (Ctrl+d)">
+                            <i class="ion ion-ios-list-outline" ></i> <?= cclang('save_and_go_the_list_button'); ?>
+                            </a>
+                            <a class="btn btn-flat btn-default btn_action" id="btn_cancel" title="cancel (Ctrl+x)">
+                            <i class="fa fa-undo" ></i> <?= cclang('cancel_button'); ?>
+                            </a>
+                            <span class="loading loading-hide">
+                            <img src="<?= BASE_ASSET; ?>/img/loading-spin-primary.svg"> 
+                            <i><?= cclang('loading_field_data'); ?></i>
+                            </span>
+                        </div>
+                        <?= form_close(); ?>
+                    </div>
+                </div>
+                <!--/box body -->
+            </div>
+            <!--/box -->
+        </div>
+    </div>
+</section>
+<!-- /.content -->
+<!-- Page script -->
+<script>
+    $(document).ready(function(){
+      
+             
+      $('#btn_cancel').click(function(){
+        swal({
+            title: "Are you sure?",
+            text: "the data that you have created will be in the exhaust!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes!",
+            cancelButtonText: "No!",
+            closeOnConfirm: true,
+            closeOnCancel: true
+          },
+          function(isConfirm){
+            if (isConfirm) {
+              window.location.href = BASE_URL + 'administrator/form_test';
+            }
+          });
+    
+        return false;
+      }); /*end btn cancel*/
+    
+      $('.btn_save').click(function(){
+        $('.message').fadeOut();
+            
+        var form_form_test = $('#form_form_test');
+        var data_post = form_form_test.serializeArray();
+        var save_type = $(this).attr('data-stype');
+        data_post.push({name: 'save_type', value: save_type});
+    
+        $('.loading').show();
+    
+        $.ajax({
+          url: form_form_test.attr('action'),
+          type: 'POST',
+          dataType: 'json',
+          data: data_post,
+        })
+        .done(function(res) {
+          if(res.success) {
+            var id = $('#form_test_image_galery').find('li').attr('qq-file-id');
+            if (save_type == 'back') {
+              window.location.href = res.redirect;
+              return;
+            }
+    
+            $('.message').printMessage({message : res.message});
+            $('.message').fadeIn();
+            $('.data_file_uuid').val('');
+    
+          } else {
+            $('.message').printMessage({message : res.message, type : 'warning'});
+          }
+    
+        })
+        .fail(function() {
+          $('.message').printMessage({message : 'Error save data', type : 'warning'});
+        })
+        .always(function() {
+          $('.loading').hide();
+          $('html, body').animate({ scrollTop: $(document).height() }, 2000);
+        });
+    
+        return false;
+      }); /*end btn save*/
+      
+           
+    
+    }); /*end doc ready*/
+</script>
